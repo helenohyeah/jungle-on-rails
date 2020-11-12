7912af08802578fc85a6498bbedb7d1cedb95cf0
helenohyeah <ouyang.helen@gmail.com>
2020-11-12 15:47:19 +0000

    Create helper methods to track login state

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,18 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  
+  # If user is logged in, save their user object
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+  helper_method :current_user # Allows method to be used in view files
+
+  # Send visitor to login page if they aren't login to keep specific pages secure
+  def authorize
+    redirect_to '/login' unless current_user
+  end
+  # add 'before_filter :authorize' to any controllers that need to be secured
 
   private
 
