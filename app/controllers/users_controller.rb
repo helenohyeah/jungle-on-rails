4f4c57ca37b1fc8f1e29fceeba4b7b5a7ef7a55d
helenohyeah <ouyang.helen@gmail.com>
2020-11-12 15:27:01 +0000

    Implement create user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,30 @@
 class UsersController < ApplicationController
 
   def new
-      @user = User.new
+    @user = User.new
   end
 
   def create
+    @user = User.new(user_params)
+
+    if @user.save
+      session[:user_id] = user_id
+      redirect_to '/'
+    else
+      render '/signup'
+    end
+  end
+
+  private
+
+  def user_params
+    paras.require(:user).permit(
+      :first_name,
+      :last_name,
+      :email,
+      :password,
+      :password_confirmation
+    )
   end
 
 end
