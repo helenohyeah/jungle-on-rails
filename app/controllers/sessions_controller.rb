cdf95cceb4e49bb6b8e1e47fe766c160e8d978e0
helenohyeah <ouyang.helen@gmail.com>
2020-11-12 23:20:42 +0000

    Fix issue with login form

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,23 +5,23 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(params[:email])
+    user = User.find_by_email(params[:email])
     # If the user exists and the password is correct
-    puts "here>>>> #{params[:password]}"
-    if user && user.authenticate('password')
+    if user && user.authenticate(params[:password])
+      puts "user>>>> #{user.inspect}"
       # Save the user_id inside browser cookie
       session[:user_id] = user.id
       redirect_to '/'
     # If login fails, send them back to login form
     else
-      render :new
+      redirect_to '/login'
     end
   end
 
   def destroy
     # Logout user and clear browser cookie
     session[:user_id] = nil
-    redirect_to '/'
+    redirect_to '/login'
   end
 
 end
