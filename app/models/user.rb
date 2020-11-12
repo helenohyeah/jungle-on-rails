90a60a376b3b53f089939b4eb95ed0034e7cdef6
helenohyeah <ouyang.helen@gmail.com>
2020-11-12 14:58:13 +0000

    Fix email validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,16 @@
 class User < ActiveRecord::Base
 
-  REGEX_PATTERN = /^[\\w!#$%&'*+/=?`{|}~^-]+(?:\\.[\\w!#$%&'*+/=?`{|}~^-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$/
+  REGEX_PATTERN = /\z(.+)@(.+)\z/
 
   has_many :order
 
   validates :first_name, presence: true
   validates :last_name, presence: true
-  validates :email, presence: true, format: { with: REGEX_PATTERN, message: "Email invalid" } 
+  validates :email,
+    presence: true,
+    format: { with: REGEX_PATTERN, message: "Email invalid" },
+      uniqueness: { case_sensitive: false },
+      length: { minimum: 4, maximum: 254 }
   validates :password_digest, presence: true
 
 end
