b2e6f0467c2a4714c2b5a1fe372d288be338d6a8
helenohyeah <ouyang.helen@gmail.com>
2020-11-12 17:27:22 +0000

    Fix email regex pattern

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
   has_secure_password
   has_many :order
   
-  REGEX_PATTERN = /\z(.+)@(.+)\z/
+  REGEX_PATTERN = /(.+)@(.+)/
 
   validates :first_name, presence: true
   validates :last_name, presence: true
