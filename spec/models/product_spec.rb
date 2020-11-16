ddd3dfb563aba2825bb0627c125c7cddc77aad7a
helenohyeah <ouyang.helen@gmail.com>
2020-11-16 20:59:12 +0000

    Create empty tests for each case

---
+++ spec/models/product_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe Product, type: :model do
+
+  describe 'Validations' do
+    
+    it "saves successfully given a product with all fields filled" 
+    end
+
+    it "returns missing name error if the name field is nil"
+    end
+
+    it "returns missing price error if the price field is nil"
+    end
+
+    it "returns missing quantity error if the quantity field is nil"
+    end
+    
+    it "returns missing category error if the category field is nil"
+    end
+
+  end
+  
+end
